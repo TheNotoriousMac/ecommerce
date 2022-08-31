0d9433175e6ca1d8ca6f16994858df1fd9c1a5a1
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-08-31 17:38:09 -0400

    Finish Product Page.

---
+++ src/pages/Product/Product.jsx
@@ -0,0 +1,81 @@
+import {
+  Container,
+  Wrapper,
+  ImgContainer,
+  Image,
+  InfoContainer,
+  Title,
+  Desc,
+  Price,
+  FilterContainer,
+  Filter,
+  FilterTitle,
+  FilterColor,
+  FilterSize,
+  FilterSizeOption,
+  AddContainer,
+  AmountContainer,
+  Amount,
+  Button,
+} from "./Product.styles";
+import Navbar from "../../components/Navbar/Navbar";
+import Announcement from "../../components/Announcement/Announcement";
+import Newsletter from "../../components/Newsletter/Newsletter";
+import Footer from "../../components/Footer/Footer";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+const Product = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
+            iaculis nulla. Aenean bibendum fringilla est et tempus. Curabitur
+            sed lorem non orci viverra interdum. Vivamus vestibulum massa massa,
+            ac tincidunt erat lobortis in. Aliquam quis ante nisl. Donec rhoncus
+            tortor sit amet lobortis elementum. Nulla finibus sit amet elit.
+          </Desc>
+          <Price>$ 20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+                <RemoveIcon />
+                <Amount>1</Amount>
+                <AddIcon />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
